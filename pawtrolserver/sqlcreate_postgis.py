639da931a1004c38bc3e8c1e9b60949a802d8562
Aaron Romeo <aaron.romeo@gmail.com>
2015-02-22 00:24:26 -0500

    Switching to use the created database.

--- pawtrolserver/sqlcreate_postgis.py
+++ pawtrolserver/sqlcreate_postgis.py
@@ -6,5 +6,6 @@ import os
 print "CREATE USER {} WITH ENCRYPTED PASSWORD '{}' CREATEDB;".format(os.getenv('DB_USER'), os.getenv('DB_PASS'))
 print "CREATE DATABASE {} WITH ENCODING 'UTF-8' OWNER {};".format(os.getenv('DB_NAME'), os.getenv('DB_USER'))
 print "GRANT ALL PRIVILEGES ON DATABASE {} TO {};".format(os.getenv('DB_NAME'), os.getenv('DB_USER'))
+print "\c {}".format(os.getenv('DB_NAME'))
 print "CREATE EXTENSION postgis;"
 print "CREATE EXTENSION postgis_topology;"
