cce8d00d0b6208c0b8f2a12df49db610256cdc6b
Aaron Romeo <aaron.romeo@gmail.com>
2015-02-21 22:44:33 -0500

    Syntax correction.

--- pawtrolserver/sqlcreate_postgis.py
+++ pawtrolserver/sqlcreate_postgis.py
@@ -4,7 +4,7 @@ import os
 # is causing Django to check the database before execution
 
 print "CREATE USER {} WITH ENCRYPTED PASSWORD '{}' CREATEDB;".format(os.getenv('DB_USER'), os.getenv('DB_PASS'))
-print "CREATE DATABASE {} WITH ENCODING 'UTF-8' OWNER '{}';".format(os.getenv('DB_NAME'), os.getenv('DB_USER'))
+print "CREATE DATABASE {} WITH ENCODING 'UTF-8' OWNER {};".format(os.getenv('DB_NAME'), os.getenv('DB_USER'))
 print "GRANT ALL PRIVILEGES ON DATABASE {} TO {};".format(os.getenv('DB_NAME'), os.getenv('DB_USER'))
 print "CREATE EXTENSION postgis;"
 print "CREATE EXTENSION postgis_topology;"
